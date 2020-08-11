610b90d7969cc3e9892738e2414bdf546d5f9fad
Tyler Linen <66691586+tylerlinen@users.noreply.github.com>
2020-08-11 18:18:42 -0400

    up

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //on click function
+$(document).ready(function(){
 $("#searchBtn").on("click", function (event) {
   event.preventDefault();
   $("#today").empty();
@@ -8,7 +9,8 @@ $("#searchBtn").on("click", function (event) {
   var city = $("#citySearch").val();
   console.log(city);
 
-  getWeather(city)
+  getWeather(city);
+  makeRow(city)
 
 });
 
@@ -35,33 +37,24 @@ function getWeather(city) {
 
       var cardIcon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
 
-      var temp = $("<h3>").text(response.main.temp);
-      var wind = $("<h3>").text(response.wind.speed);
-      var humid = $("<h3>").text(response.main.humidity);
+      var temp = $("<h3>").text("Temp is: " + response.main.temp);
+      var wind = $("<h3>").text("Wind Speed is:" + response.wind.speed);
+      var humid = $("<h3>").text("Humudity is:" + response.main.humidity);
 
 
+    
+      $("#today").append(card.append(cardBody.append(cardTitle.append(cardIcon), temp, wind, humid,)))
 
 
-      cardBody.append(temp)
-      cardBody.append(wind)
-      cardBody.append(humid)
-
-
-
-      cardTitle.append(cardIcon)
-      $("#today").append(card.append(cardBody.append(cardTitle)))
-
-
-      getDates(response.coord.lat, response.coord.lon)
       getUVI(response.coord.lat, response.coord.lon)
-      getForecast(city)
+      getForecast(response.coord.lat, response.coord.lon)
     });
 }
 
-function getForecast(searchValue) {
+function getForecast(lat,lon) {
   var APIKey = "e2284c98c4a79fae5fbaee15f0e63c67";
 
-  var queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=${APIKey}&units=imperial`;
+  var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`;
 
   $.ajax({
     url: queryURL,
@@ -69,6 +62,18 @@ function getForecast(searchValue) {
     dataType: "json",
     success: function (response) {
       console.log(response)
+      for(var i = 1; i < 6; i++) {
+        console.log(response.daily[i])
+
+
+        var time = moment.unix(response.daily[i].dt).format("MM DD YYYY")
+        var card = $("<div>").addClass("card");
+        var cardBody = $("<div>").addClass("card-body");
+        var title = $("<h1>").addClass("card-title").text(time)
+        
+
+         $("#forecast").append(card.append(cardBody.append(title.append("Temp:" + response.daily[i].temp.day,"Humidity:" + response.daily[i].humidity))))
+      }
 
 
 
@@ -87,7 +92,7 @@ function getUVI(lat, lon) {
     method: "GET",
     dataType: "json",
     success: function (response) {
-      console.log(response.current.uvi)
+      console.log(response)
       var uv = $("<h3>").text(response.current.uvi);
 
       $("#today").append(uv);
@@ -102,39 +107,16 @@ function getUVI(lat, lon) {
     }
   });
 }
-function getDates(lat, lon) {
-  var APIKey = "e2284c98c4a79fae5fbaee15f0e63c67";
 
-  // Here we are building the URL we need to query the database
-  var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}&units=imperial`;
-
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-    dataType: "json",
-    success: function (response) {
-
-      //Times and Dates
-      for (var i = 1; i < 6; i++) {
-        console.log(response.daily[i])
-
-
-        var card2 = $("<div>").addClass("card");
-        var cardBody2 = $("<div>").addClass("card-body");
-        var cardTitle2 = $("<h2>").addClass("card-title").text(timeEl);
-
-        var timeEl = moment.unix(response.daily[i].dt).format("dddd, MMMM Do");
-        var timeIl = moment.unix(response.daily[0].dt).format("dddd, MMMM Do");
-
-        $("#today").append(timeIl);
-
-        $("#forecast").append(card2.append(cardBody2.append(cardTitle2)))
-
-
-      }
-    }
-  });
+function makeRow(text) {
+  var li = $("<li>").addClass("list-group-item").text(text);
+  $("#cityList").append(li)
 }
+
+    
+
+
+});
 
 
 
